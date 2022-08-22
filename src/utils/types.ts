ffc3cf1e6f9cfbe6742802328b3850fe8d0e38dd
Victor Hugo <victor@hazmiapp.com>
2022-08-21 23:39:37 -0500

    add interfaces and types file

---
+++ src/utils/types.ts
@@ -0,0 +1,14 @@
+export interface Item {
+  title: string,
+  category: number,
+  color: string,
+  price: number[],
+  size?: string[],
+  flavors?: string[],
+}
+
+export interface TicketItem {
+  amount: number,
+  title: string,
+  price: number,
+}
